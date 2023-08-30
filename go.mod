954aae0f0f2656b7a104764e7adbae854f1f213a
joeycyy <1029122023@qq.com>
2023-08-30 16:50:41 +0800

    初步框架commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joeycyy/go-gee
+
+go 1.19
+
+replace gyy => ./gyy
---
+++ gyy/gyy.go
@@ -0,0 +1,42 @@
+package gyy
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New 是gyy的构造器
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	e.router[key] = handler
+}
+
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, e)
+}
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(w, r)
+	} else {
+		fmt.Fprintf(w, "404 Not Found: %s\n", r.URL)
+	}
+}
